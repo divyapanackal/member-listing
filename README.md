baa139979a34038b631a0964ffbdaa81e8723577
divyap <divyap@qburst.com>
2025-06-26 14:45:41 +0530

    Add/edit/delete

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # member-listing
+# member listing with Add, Delete and edit option. On clicking Add show a form with fields below
+# Name – Only letters, min 3 chars
+# Email – Must be a valid email format
+# Age – Must be between 18 and 99
+# Phone – Must be 10-digit number only
+# Password – Min 6 chars, must include number and special character
+# Gender – Required (radio)
+# Terms and Conditions – Must be checked
+# DB aayi Firebase
--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 fetch('/json/data.json')
   .then(response => response.json()) 
   .then(data => {
-    console.log(data);
+    // console.log(data);
 
 
     const container = document.querySelector('#membersTable tbody');
     const addButton = document.querySelector('.btn-add');
+    const submitButton = document.querySelector('#btn-submit');
+    const form = document.querySelector('#add-new-form');
 
 
     data.members.forEach((member, index) => {
@@ -39,14 +41,48 @@ fetch('/json/data.json')
           const cells = row.querySelectorAll('td');
           for(let i=0; i<5; i++) {
             const text = cells[i].textContent;
-            cells[i].innerHTML = `<input class="border border-indigo-600 p-2" type="text" value="${text}" />`;
+            // cells[i].innerHTML = `<input class="border border-indigo-600 p-2" type="text" value="${text}" />`;
+
+            switch(i) {
+              case 0:
+                cells[i].innerHTML = `<input class="border border-indigo-600 p-2" type="text" value="${text}" />`;
+                break;
+              case 1:
+                cells[i].innerHTML = `<input class="border border-indigo-600 p-2" type="email" value="${text}" />`;
+                break;
+              case 2:
+                cells[i].innerHTML = `<input class="border border-indigo-600 p-2" tel="number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value="${text}" />`;
+                break;
+              case 3:
+                cells[i].innerHTML = `<input class="border border-indigo-600 p-2" type="number" value="${text}" min="1" max="90" />`;
+                break;
+              case 4:
+                  cells[i].innerHTML = `<input type="radio" id="male-${row.i}" name="gender-${row.i}" value="Male" ${text === 'Male' ? 'checked' : ''}>
+                                         <label for="male-${row.i}">Male</label><br>
+                                        <input type="radio" id="female-${row.i}" name="gender-${row.i}" value="Female" ${text === 'Female' ? 'checked' : ''}>
+                                        <label for="female-${row.i}">Female</label>`;
+                  break;
+
+
+
+            }
           }
           target.textContent = 'Save';
         }
         else {
-            const input = row.querySelectorAll('input');
-            input.forEach((input, i) => {
-              row.children[i].textContent = input.value;
+            const cells = row.querySelectorAll('td');
+            cells.forEach((cell, i) => {
+              const input =  cell.querySelector('input');
+              if(i == 4) {
+                const checkedRadio = cell.querySelector('input[type="radio"]:checked');
+                cell.textContent = checkedRadio ? checkedRadio.value : '';
+              }
+              else  {
+                if(input) {
+                  cell.textContent = input.value;
+                }
+
+              }
             })
             target.textContent = 'Edit';
         }
@@ -59,20 +95,21 @@ fetch('/json/data.json')
 
 
       if(target.classList.contains('btn-new-save')) {
+        // debugger
         if(target.textContent === 'Save') {
-          for(let i=0; i<5; i++) {
+          console.log("here")
+          for(let i=0; i<6; i++) {
             const input = row.cells[i].querySelector('input');
-            console.log("input:"+input)
             if(input) {
               row.cells[i].textContent = input.value;
             }
           }
+          target.textContent = 'Edit';
+          target.classList.remove('btn-new-save');
+          target.classList.add('btn-edit');
         }
       }
 
-      target.textContent = 'Edit';
-      target.classList.remove('btn-new-save');
-      target.classList.add('btn-edit');
 
 
 
@@ -80,21 +117,81 @@ fetch('/json/data.json')
 
 
     addButton.addEventListener('click', function(e) {
-      const newRow = document.createElement('tr');
-      newRow.innerHTML = `
-      <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="text" placeholder="Name" /></td>
-      <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="text" placeholder="Email" /></td>
-      <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="text" placeholder="Phone" /></td>
-      <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="text" placeholder="Age" /></td>
-      <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="text" placeholder="Gender" /></td>
-       <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><button class="me-2 btn-new-save text-blue-600/100 cursor-pointer">Save</button><button class="btn-delete text-red-500 cursor-pointer">Delete</button></td>`;
 
-      container.appendChild(newRow);
+      const addForm = document.querySelector('.form-add');
+      addForm.classList.add('show-form');
+      
+      // const newRow = document.createElement('tr');
+      // const lastIndex = container.lastChild.getAttribute('data-index');
+      // const indexVal = parseInt(lastIndex) + 1;
+
+      // console.log(indexVal);
+      // debugger;
+      // newRow.innerHTML = `
+      // <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="text" placeholder="Name" /></td>
+      // <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="email" placeholder="Email" /></td>
+      // <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="tel" placeholder="Phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/></td>
+      // <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="number" placeholder="Age" min="1" max="90" /></td>
+      // <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input class="border border-indigo-600 p-2" type="password" placeholder="Password"  /></td>
+      // <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><input type="radio" id="male-${indexVal}" name="gender-${indexVal}" value="Male">
+      //                                     <label for="male-${indexVal}">Male</label><br><input type="radio" id="female-${indexVal}" name="gender-${indexVal}" value="Female"><label for="female-${indexVal}">Female</label></td>
+      //  <td class="border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400"><button class="me-2 btn-new-save text-blue-600/100 cursor-pointer">Save</button><button class="btn-delete text-red-500 cursor-pointer">Delete</button></td>`;
+
+      // container.appendChild(newRow);
 
     });
 
 
 
+    form.addEventListener('submit', function(e) {
+
+      e.preventDefault();
+      
+      var name = document.getElementById('first_name').value;
+      var phone = document.getElementById('phone').value;
+      var email = document.getElementById('email').value;
+      var password =  document.getElementById('password').value;
+      var age = document.getElementById('age').value;
+      var gender = document.querySelector('input[name="gender"]:checked');
+      var agree = document.getElementById('remember').checked;
+
+      var newRow = document.createElement('tr');
+      const lastIndex = container.lastChild.getAttribute('data-index');
+      const indexVal = parseInt(lastIndex) + 1;
+      newRow.setAttribute('data-index', indexVal);
+
+      function addCell(value) {
+        var cell = document.createElement('td');
+          cell.classList.add(...'border-b border-gray-100 p-4 pl-8 text-gray-500 dark:border-gray-700 dark:text-gray-400'.split(' '));
+
+        if (value === 'actions') {
+          cell.innerHTML = '<button class="me-2 btn-edit text-blue-600/100 cursor-pointer">Edit</button><button class="btn-delete text-red-500 cursor-pointer">Delete</button>'
+        }
+        else {
+
+          cell.textContent = value;
+        }
+        newRow.appendChild(cell);
+      }
+
+
+      addCell(name);
+      addCell(email);
+      addCell(phone);
+      // addCell(password);
+      addCell(age);
+      addCell(gender.value);
+      // addCell(agree ? 'yes' : 'No');
+      addCell('actions');
+
+
+
+      container.appendChild(newRow);
+      form.reset();
+      form.classList.remove('show-form');
+
+
+    });
 
 
 
